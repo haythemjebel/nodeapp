3fdd6f5cd5a97c053130dd5f6a249c6d1d383152
haythem <haythemjebel@gmail.com>
2020-03-21 11:40:30 +0100

    creating a node server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyparse = require('body-parser')
+
+mongoose.connect('mongodb://localhost:27017/testdb', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (err) => {
+    console.log(err)
+})
+db.once('open', () => {
+    console.log('Database connection established !')
+})
+const app = express()
+app.use(morgan('dev'))
+app.use(bodyparse.urlencoded({ extended: true }))
+app.use(bodyparse.json())
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
